7435447d653df0184e538aa53403798196e9f217
krukle <christofferfareseid@gmail.com>
2023-04-29 10:50:41 +0200

    document main class

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
 from dataloader import DataLoader
+from task import Task
 from task1 import Task1
 from task2 import Task2
 
 class Main():
     """
     Main
+
+    Main class
+
+    Attributes:
+        data_loader (DataLoader): data loader
+        data (list): data
+        tasks (list): tasks
     """
+    
     def __init__(self):
+        """
+        Initialize the main class
+        """
         self.data_loader = DataLoader()
         self.data_loader.load('data/input.txt')
         self.data = self.data_loader.get()
@@ -14,9 +26,12 @@ class Main():
         for task in self.tasks:
             self.run_task(task)
 
-    def run_task(self, task:Task1):
+    def run_task(self, task:Task):
         """
-        Run
+        Run task
+
+        Args:
+            task (Task): task
         """
         print (f'Running {task.get_name()} - {task.get_description()}...')
         task.solve()
