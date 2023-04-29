3a9517c81c8da851bfcd78a1ec292eafee9b2583
krukle <christofferfareseid@gmail.com>
2023-04-29 10:51:14 +0200

    document task 1 class

--- src/task1.py
+++ src/task1.py
@@ -3,15 +3,34 @@ from task import Task
 class Task1(Task):
     """
     Task 1
+
+    Sum of all numbers in the file
+    
+    Args:
+        data (list): data
+    
+    Attributes:
+        data (list): data
+        result (int): result
+        name (str): name
+        description (str): description
     """
     def __init__(self, data:list):
+        """
+        Initialize the task
+
+        Args:
+            data (list): data
+        """
         super().__init__(data)
         self.name = 'Task 1'
         self.description = 'Sum of all numbers in the file'
 
     def solve(self):
         """
-        Solve the task    
+        Solve the task  
+        
+        Sum of all numbers in the file
         """
         self.result = sum(self.data)
         
